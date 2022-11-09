d7bd9c0c47f09cbf661b53605a04130bb1a477f6
juone00 <juwon0208@naver.com>
2022-11-09 14:57:22 +0900

    [PRG] 게임 맵 최단거리 Lv.2

--- lv_02/gama_map_shortest.py
+++ lv_02/gama_map_shortest.py
@@ -2,11 +2,10 @@
 
 from collections import deque
 
-
-
-def bfs(map):
+def bfs(maps):
     
-    M = len(map) - 1
+    N = len(maps) - 1 # 세로
+    M = len(maps[0]) - 1 # 가로
     dx = [0, 1, 0, -1] # 4방향 북동남서 순서
     dy = [1, 0, -1, 0]
 
@@ -16,24 +15,23 @@ def bfs(map):
     while q:
         a,b = q.popleft()
         
-        if a == M and b == M:
-            print(map[a][b])
+        if a == N and b == M: # 끝에 도달하면
+            print(maps[a][b])
         
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
             
-            if 0 <= nx <= M and 0 <= ny <= M and map[nx][ny] == 1:
+            if 0 <= nx <= N and 0 <= ny <= M and maps[nx][ny] == 1: # 범위안에서 1이면(벽이 아니면)
                 q.append([nx,ny])
-                map[nx][ny] = map[a][b] + 1
+                maps[nx][ny] = maps[a][b] + 1 # 앞으로 나아갈 수 있으면 (그 전의 값)+1 한 값을 나아갈 곳에 저장함
     
-    if map[M][M] == 1:
+    if maps[N][M] == 1: # 끝에 도달하지 못하면
         print(-1)
     
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1],[0,0,0,0,1]]	
 
-map = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	
+bfs(maps)
 
-bfs(map)
+# print(maps)
 
-
-print(map)
